6e2eac5838edfaabc38615a7bddb7ebf2f5e464a
Michael Dixon <qualitydixon@gmail.com>
2016-06-24 11:09:56 -0600

    Slight code refactoring and css adjustments.

--- app/components/GifCard.js
+++ app/components/GifCard.js
@@ -1,6 +1,17 @@
 import React, { PropTypes, Component } from 'react'
 import { shareToTwitter, shareToFacebook } from '../utils/helpers'
 
+const GifBtn = ({ link, animation, iconType }) =>
+  <a href={link} target='_blank'>
+    <i className={`icon fa fa-${iconType} ${iconType} ${animation}`}></i>
+  </a>
+
+GifBtn.propTypes = {
+  link: PropTypes.any,
+  iconType: PropTypes.string.isRequired,
+  animation: PropTypes.string.isRequired,
+}
+
 export default class GifCard extends Component {
   constructor(props) {
     super(props)
@@ -20,9 +31,9 @@ export default class GifCard extends Component {
   }
   render() {
     const animationDelay = `${2 + (this.props.idx * 10) / 100}s`
-    let isVisible = this.state.isHovered ? 'fadeIn' : 'fadeOut'
-    let displayURL = this.state.isHovered ? this.props.gifURL : this.props.stillURL
-    let enlarge = this.state.isHovered ? 'enlarge' : ''
+    const animation = this.state.isHovered ? 'fadeIn' : 'fadeOut'
+    const displayURL = this.state.isHovered ? this.props.gifURL : this.props.stillURL
+    const enlarge = this.state.isHovered ? 'enlarge' : ''
     return (
       <li
         onMouseOver={() => this.mouseOver()}
@@ -30,18 +41,13 @@ export default class GifCard extends Component {
         style={{ animationDelay }}
         className={`card ${enlarge}`}
       >
-        <a href={this.props.pageURL} target='_blank'>
-          <img
-            alt={this.props.gifURL}
-            src={displayURL}
-          />
-        </a>
-        <a href={shareToTwitter(this.props.shareURL)} target='_blank'>
-          <i className={`fa fa-twitter icon twitter ${isVisible}`}></i>
-        </a>
-        <a href={shareToFacebook(this.props.shareURL)} target='_blank'>
-          <i className={`fa fa-facebook icon facebook ${isVisible}`}></i>
-        </a>
+        <img
+          alt={this.props.gifURL}
+          src={displayURL}
+        />
+        <GifBtn animation={animation} iconType={'external-link'} link={this.props.pageURL} />
+        <GifBtn animation={animation} iconType={'twitter'} link={shareToTwitter(this.props.shareURL)} />
+        <GifBtn animation={animation} iconType={'facebook'} link={shareToFacebook(this.props.shareURL)} />
       </li>
     )
   }
